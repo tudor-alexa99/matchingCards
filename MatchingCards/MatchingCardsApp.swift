0590ce1b185992b95a7c675498d9bc4a64461a90
Tudor Alexa <tudor-alexa@bitbucket.org>
2023-08-31 19:26:35 +0300

    Initial Commit

---
+++ MatchingCards/MatchingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingCardsApp.swift
+//  MatchingCards
+//
+//  Created by Tudor Alexa on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MatchingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
